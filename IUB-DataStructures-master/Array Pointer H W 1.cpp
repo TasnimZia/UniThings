980278b9993d3945e48fbd478a97bf2d32cdca80
ScarletMcLearn <ratul_shams@yahoo.com>
2017-03-06 20:07:50 +0600

    Add files via upload

---
+++ IUB-DataStructures-master/Array Pointer H W 1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+#include <cstdlib>
+#include <ctime>
+
+
+using namespace std;
+
+int main()
+{
+
+         cout<<"Please enter rows, columns, and pages." <<endl;
+
+         cout<<"Rows: ";
+         int rows;
+         cin>>rows;
+
+         cout<<"Columns: ";
+         int columns;
+         cin>> columns;
+
+         cout<<"Pages: ";
+         int pages;
+         cin>> pages;
+
+
+         int Array[rows][columns][pages];
+
+	       srand(time(0));
+
+           // for (int j = 0; j<=10; j++ )
+           // {
+           //     cout<< 0 + rand() % (10 + 1) <<" ";
+           // }
+
+        for (int iter = 0; iter < pages; iter++)
+        {
+          int odd_counter = 0;
+
+          cout<<"Page " << iter+1 <<": "<<endl;
+            for (int jiter = 0; jiter < columns ; jiter++)
+            {
+
+              for (int piter = 0; piter < rows; piter++)
+              {
+                  Array[piter][jiter][iter] = 10 + rand() % (99-10+1); 
+
+                  cout<<Array[piter][jiter][iter] << " ";
+
+                  if (Array[piter][jiter][iter] % 2 != 0)
+                  {
+                    odd_counter++;
+                  }
+              }
+
+              cout<<endl;
+
+            }
+
+cout<<"Odd Counter: " << odd_counter<<endl<<endl;
+           // cout<<endl;
+        }
+
+
+//           srand(time(0));
+
+           for (int j = 0; j<=10; j++ )
+           {
+               cout<< 0 + rand() % (10 + 1) <<" ";
+           }
+
+           
+
+
+	return 0;
+}
---
+++ IUB-DataStructures-master/Array Ponter 3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float arr[5];
+    
+    cout<<"Displaying address using pointers notation: "<< endl;
+    for (int i = 0; i < 5; ++i) {
+        cout << arr + i <<endl;
+    }
+
+    return 0;
+}
